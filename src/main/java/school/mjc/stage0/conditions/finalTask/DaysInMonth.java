a1d7570a52d8eeda7f0db304a0744e640b4c6994
Madinabonu <otkirbekovamadinabonu@gmail.com>
2022-08-12 11:02:36 +0300

    .

--- src/main/java/school/mjc/stage0/conditions/finalTask/DaysInMonth.java
+++ src/main/java/school/mjc/stage0/conditions/finalTask/DaysInMonth.java
@@ -3,5 +3,16 @@ package school.mjc.stage0.conditions.finalTask;
 public class DaysInMonth {
     public void printDays(int year, int month) {
 
+        if(month>=1&&month<=12&&year>0){
+            if(((month<8&&month%2!=0))||(month>=8&&month%2==0))
+                System.out.println(31);
+            if(((month<8&&month%2==0&&month!=2))||(month>8&&month%2!=0))
+                System.out.println(30);
+            if(month==2)
+                System.out.println(28);}
+        else
+            System.out.println("invalid date");
+
+
     }
 }
